bbafa8908261d26c238a08fee1be0b3d575e62c8
Brandon Flores <32563181+BrandonJLFlores@users.noreply.github.com>
2017-12-11 21:18:43 +0800

    FUCK!

--- hang.asm
+++ hang.asm
@@ -1,55 +1,61 @@
+TITLE HANGMAN (SIMPLFIED .EXE FORMAT)
 .MODEL SMALL
+;---------------------------------------------
 .STACK 100H
-
-
-
+;---------------------------------------------
 .DATA
+	TEMP DB 32 DUP('$') 
 
-TEMP DB ?,'$'
+	PROMPT3 DB 0DH,0AH,'What letter do you guess?  ', '$'
+	PROMPT4 DB 0DH,0AH,'The word is:',0DH,0AH, '$'
+	PROMPT5 DB 0DH,0AH,'Congratulations! You won the game',0DH,0AH, '$'
+	PROMPT6 DB 0DH,0AH,'Sorry you missed.',0DH,0AH, '$'
+	PROMPT7 DB 0DH,0AH,'Sorry, duplicate entry.  Please try again.',0DH,0AH, '$'
+	PROMPT8 DB 0DH,0AH,'List of chosen letter(s): ', '$'
+	CRLF   DB  0DH, 0AH, '$'
+	NEWLINE DB 10,13,'$'
+	
+	FIG0    DB 0DH,0AH,' +=======+',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG1    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG2    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/        |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG3    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/ \      |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG4    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG5    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG6    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'/        |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG7    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'/ \      |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
+	FIG8    DB 0DH,0AH,' +=======+',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'   \O/   |',0DH,0AH,'    |    |',0DH,0AH,'    |    |',0DH,0AH,'   / \   |',0DH,0AH,' ========+',0DH,0AH,'$'
+	
+	ERROR1_STR    DB 'Error in opening file.$'
+  ERROR2_STR    DB 'Error reading from file.$'
+  ERROR3_STR    DB 'No record read from file.$'
+	
+	PATHFILENAME  DB 'file.txt', 00H
+  FILEHANDLE    DW ?
 
-PROMPT1 DB 0DH,0AH,'Are you ready to play? (y for YES, n for NO):', '$'
-PROMPT2 DB 0DH,0AH,'Please try again ', '$'
-PROMPT3 DB 0DH,0AH,'What letter do you guess?  ', '$'
-PROMPT4 DB 0DH,0AH,'The word is:',0DH,0AH, '$'
-PROMPT5 DB 0DH,0AH,'Congratulations! You won the game',0DH,0AH, '$'
-PROMPT6 DB 0DH,0AH,'Sorry you missed.',0DH,0AH, '$'
-PROMPT7 DB 0DH,0AH,'Sorry, duplicate entry.  Please try again.',0DH,0AH, '$'
-PROMPT8 DB 0DH,0AH,'List of chosen letter(s): ', '$'
-CRLF   DB  0DH, 0AH, '$'
-
-FIG0    DB 0DH,0AH,' +=======+',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG1    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG2    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/        |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG3    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/ \      |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG4    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG5    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG6    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'/        |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG7    DB 0DH,0AH,' +=======+',0DH,0AH,' |       |',0DH,0AH,' O       |',0DH,0AH,'/|\      |',0DH,0AH,' |       |',0DH,0AH,'/ \      |',0DH,0AH,'         |',0DH,0AH,' ========+',0DH,0AH,'$'
-FIG8    DB 0DH,0AH,' +=======+',0DH,0AH,'         |',0DH,0AH,'         |',0DH,0AH,'   \O/   |',0DH,0AH,'    |    |',0DH,0AH,'    |    |',0DH,0AH,'   / \   |',0DH,0AH,' ========+',0DH,0AH,'$'
-
-STACK_INPUT DB ?,'$'
-
-PROMPT DB 0DH, 0AH,'Please Enter The Message Number (0-9) ', '$'
-ERROR DB 0DH, 0AH,'Input Must be a digit. Please enter again:', 0DH, 0AH,'$' 
-MSG0 DB  'temperature$'
-MSG1 DB  'ceremony$'
-MSG2 DB  'paradise$'
-MSG3 DB  'memorial$'
-MSG4 DB  'wonderful$'
-MSG5 DB  'substance$'
-MSG6 DB  'animation$'
-MSG7 DB  'boutique$'
-MSG8 DB  'dangerous$'
-MSG9 DB  'element$'
-
-CREDIT    DB ?
-correct   DB ?
-incorrect DB ?
-STR_L	  DB ?
-
-MSG  DB ?,'$'
-
+	STACK_INPUT		DB 32 DUP('$') 
+	RECORD_STR    DB 32 DUP('$')  ;length = original length of record + 1 (for $)
+  TOBESOLVED    DB 32 DUP(' ')  ;length = original length of record + 1 (for $)
+	
+	
+	;TEMP DB ?,'$'
+	;STACK_INPUT DB ?,'$'
+	CREDIT    DB ?
+	correct   DB ?
+	incorrect DB ?
+	STR_L	  DB ?
+	;MSG  DB ?,'$'
+	MSG DB 32 DUP('$') 
+	
+	COUNT DW ?
+	COUNT2 DB 0
+	TEMPSI DW ?
+	
+  
+;---------------------------------------------
 .CODE
+;*************************************************************************
+;  MACRO FOR PRINTING STRINGS
+;*************************************************************************
 PRINTSTR MACRO STRING
 
 MOV AH,9
@@ -57,222 +63,152 @@ LEA DX, STRING
 INT 21H
 ENDM
 
-COPYMESSAGE MACRO MESSAGE
-LOCAL COPY
-LOCAL QUITS
+PRINTCH MACRO STRING
+
+MOV AH,2
+MOV DL,AL
+INT 21H
+ENDM
+
+MAIN PROC FAR
+  MOV AX, @data
+  MOV DS, AX
+	XOR BP,BP
+	LEA SI, RECORD_STR
+	LEA DI, TOBESOLVED
+	MOV BP,SI
+	
+  ;MAINGAME:
+;*************************************************************************
+;  FILE READING
+;*************************************************************************
+  ;open file
+  MOV AH, 3DH           ;requst open file
+  MOV AL, 00            ;read only; 01 (write only); 10 (read/write)
+  LEA DX, PATHFILENAME
+  INT 21H
+  JC DISPLAY_ERROR1
+  MOV FILEHANDLE, AX
+
+  ;read file
+  MOV AH, 3FH           ;request read record
+  MOV BX, FILEHANDLE    ;file handle
+  MOV CX, 1000            ;record length
+  LEA DX, RECORD_STR    ;address of input area
+  INT 21H
+  JC DISPLAY_ERROR2
+  CMP AX, 00            ;zero bytes read?
+  JE DISPLAY_ERROR3
+  
+	
+	
+	CALL GETCOUNT ;gets file string length
+	
+	;PLAY:
+	CALL GETLINE ; gets 1 line form file
+	
+  ;display record
+		PRINTSTR TOBESOLVED
+	
+	CALL TABLE
+	CALL matchWord
+	
+	;PRINTSTR NEWLINE
+	
+	;LEA DI, TOBESOLVED
+	
+	;CMP COUNT,0
+	;JNE PLAY
+	
+	;close file handle
+  MOV AH, 3EH           ;request close file
+  MOV BX, FILEHANDLE    ;file handle
+  INT 21H
+	;JMP MAINGAME
+	
+  JMP EXIT
+
+DISPLAY_ERROR1:
+  LEA DX, ERROR1_STR
+  MOV AH, 09
+  INT 21H
+
+  JMP EXIT
+
+DISPLAY_ERROR2:
+  LEA DX, ERROR2_STR
+  MOV AH, 09
+  INT 21H
+
+  JMP EXIT
+
+DISPLAY_ERROR3:
+  LEA DX, ERROR3_STR
+  MOV AH, 09
+  INT 21H
+
+EXIT:
+  MOV AH, 4CH
+  INT 21H
+MAIN ENDP
+
+GETCOUNT PROC
+	MOV CX,AX
+	MOV COUNT,CX
+	RET
+GETCOUNT ENDP
+
+GETLINE PROC
+	MOV SI,BP
+	MOV TEMPSI, SI
+
+	ITERATE:
+		MOV AL,[SI]
+		CMP AL,'*'
+		JE ENDGET
+		MOV [DI],AX
+		INC SI
+		INC DI
+		DEC COUNT
+	JMP ITERATE	
+		
+	
+	ENDGET:
+	DEC COUNT
+	MOV AL,10
+	MOV [DI],AX
+	MOV AL,13
+	MOV [DI],AX
+	MOV AL,'$'
+	MOV [DI],AX
+	INC SI
+	MOV BP,SI
+
+	RET
+GETLINE ENDP
+
+;*************************************************************************
+;  table procedure
+;*************************************************************************
+
+TABLE PROC 	
 ;copy string
 	XOR SI,SI
-COPY:
-	CMP MESSAGE[SI],'$'
-	JE QUITS
-	MOV AL,MESSAGE[SI]
+COPY0:
+	CMP TOBESOLVED[SI],'$'
+	JE END0
+	MOV AL,TOBESOLVED[SI]
 	MOV MSG[SI],AL
 	INC SI
-	LOOP COPY
-QUITS:
+	LOOP COPY0
+END0:
 	MOV MSG[SI],'$'
-    CALL displayEmpty    	
-   	;JMP END_WTABLE
-ENDM
-;***************************************************************************
-;	Main procedure
-;***************************************************************************
-MAIN	PROC
-	MOV	AX,@DATA	;initialize DS
-	MOV	DS,AX
-
-	PRINTSTR PROMPT
-	;MOV  AH, 9     		;ask user to put a number from 0-9
-
-LOOP1:
-    	MOV  AH, 1    		;read in an integer from user
-    	INT  21H  
-
-    	CMP  AL, 30H   
-    	JB   RE_ENTER
-    	CMP  AL, 39H
-    	JA   RE_ENTER
-
-    	CALL wordTable		;******* wordTable function call *******
-	CALL matchWord		;******* matchWord function call *******
-
-;ask if user wants to play again.
-	PRINTSTR PROMPT2
-	JMP ENDWHILE
-RE_ENTER:
-PRINTSTR ERROR
-    	
-
-    	JMP LOOP1
-
-ENDWHILE:
-	MOV AH,4CH
-	INT 21H			;DOS exit
-MAIN 	ENDP
-
-;*************************************************************************
-;	Display (bitmap) procedure
-;*************************************************************************
-DISPLAY PROC
-;checking score value and display
-        CMP incorrect,0
-        JE DISP0
-
-        CMP incorrect,1
-        JE DISP1
-
-        CMP incorrect,2
-        JE DISP2
-
-        CMP incorrect,3
-        JE DISP3
-
-        CMP incorrect,4
-        JE DISP4
-
-        CMP incorrect,5
-        JE DISP5
-
-        CMP incorrect,6
-        JE DISP6
-
-        CMP incorrect,7
-        JE DISP7
-
-        CMP incorrect,8
-        JE DISP8
-
-		
-RET
-;display message0
-DISP0:
-		PRINTSTR FIG0
-        RET
-
-DISP1:
-;display message1
-		PRINTSTR FIG1
-        RET
-
-DISP2:
-;display message2
-		PRINTSTR FIG2
-        RET
-
-DISP3:
-;display message3
-		PRINTSTR FIG3
-        RET
-
-DISP4:
-;display message4
-		PRINTSTR FIG4
-        RET
-
-DISP5:
-;display message5
-		PRINTSTR FIG5
-        RET
-
-DISP6:
-;display message6
-		PRINTSTR FIG6
-        RET
-
-DISP7:
-;display message7
-        PRINTSTR FIG7
-        RET
-
-DISP8:
-;display message8
-		PRINTSTR FIG8
-        RET
-
-DISPLAY ENDP
-
-;*************************************************************************
-;  wordTable procedure
-;*************************************************************************
-wordTable PROC
-        ;INPUT: an integer from 0-9 from user
-        ;OUTPUT: a coresponding message
+	;PRINTSTR MSG
+  CALL displayEmpty    	 	
 	
-    	CMP AL, 30H
-    	JE MESSAGE0
+	RET
+TABLE ENDP
 
-    	CMP AL, '1'
-    	JE MESSAGE1
-
-    	CMP AL, '2'
-    	JE MESSAGE2
-		;JE m2
-
-    	CMP AL, '3'
-    	JE MESSAGE3
-		;JE m3
-
-    	CMP AL, '4'
-    	JE MESSAGE4
-		;je m4
-
-    	CMP AL, '5'
-    	JE MESSAGE5
-		;je m5
-		
-    	CMP AL, '6'
-    	JE MESSAGE6
-		;je m6
-
-    	CMP AL, '7'
-    	JE MESSAGE7
-		;je m7
-
-    	CMP AL, '8'
-    	JE MESSAGE8
-		;je m8
-;**************************** CASE message 0 ********************************
-MESSAGE0: 	
-COPYMESSAGE MSG0
-
-;**************************** CASE message 1 ********************************
-MESSAGE1:
-COPYMESSAGE MSG1
-;**************************** CASE message 2 ********************************
-MESSAGE2:
-COPYMESSAGE MSG2
-
-;**************************** CASE message 3 ********************************
-MESSAGE3:
-COPYMESSAGE MSG3
-
-;**************************** CASE message 4 ********************************
-MESSAGE4:
-COPYMESSAGE MSG4
-
-;**************************** CASE message 5 ********************************
-MESSAGE5:
-COPYMESSAGE MSG5
-
-;**************************** CASE message 6 ********************************
-MESSAGE6:
-COPYMESSAGE MSG6
-
-;**************************** CASE message 7 ********************************
-MESSAGE7:
-COPYMESSAGE MSG7
-
-;**************************** CASE message 8 ********************************
-MESSAGE8:
-COPYMESSAGE MSG8
-
-
-;**************************** CASE message 9 ********************************   	
-;copy string
-MESSAGE9:
-COPYMESSAGE MSG9
-
-wordTable ENDP
 ;**************************************************************************
 ;	displayEmpty Procedure
 ;**************************************************************************
@@ -403,5 +339,103 @@ RESULT:
 
 matchWord ENDP
 
+;*************************************************************************
+;	Display (bitmap) procedure
+;*************************************************************************
+DISPLAY PROC
+;checking score value and display
+        CMP incorrect,0
+        JE DISP0
+
+        CMP incorrect,1
+        JE DISP1
+
+        CMP incorrect,2
+        JE DISP2
+
+        CMP incorrect,3
+        JE DISP3
+
+        CMP incorrect,4
+        JE DISP4
+
+        CMP incorrect,5
+        JE DISP5
+
+        CMP incorrect,6
+        JE DISP6
+
+        CMP incorrect,7
+        JE DISP7
+
+        CMP incorrect,8
+        JE DISP8
+
+        ret
+
+;display message0
+DISP0:
+        LEA DX,FIG0
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP1:
+;display message1
+        LEA DX,FIG1
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP2:
+;display message2
+        LEA DX,FIG2
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP3:
+;display message3
+        LEA DX,FIG3
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP4:
+;display message4
+        LEA DX,FIG4
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP5:
+;display message5
+        LEA DX,FIG5
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP6:
+;display message6
+        LEA DX,FIG6
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP7:
+;display message7
+        LEA DX,FIG7
+        MOV AH,9
+        INT 21h
+        RET
+
+DISP8:
+;display message8
+        LEA DX,FIG8
+        MOV AH,9
+        INT 21h
+        RET
+
+DISPLAY ENDP
+
 END MAIN
-	
